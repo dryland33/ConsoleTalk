53be3b427bf7e413b53855f66828f1021cb5b179
Rylan Wassem <dryland28@gmail.com>
2021-03-23 17:38:53 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleTalk
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Say something.");
+            string input = Console.ReadLine();
+            if (input.EndsWith(".") || input.EndsWith("?") || input.EndsWith("!"))
+                Console.WriteLine($"You said, \"{ input}\"");
+            else
+                Console.WriteLine($"You said, \"{ input}\".");
+        }
+    }
+}
